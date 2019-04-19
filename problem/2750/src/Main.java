f34395fdf31fe8520e3a53c4f9831e1bb7b34e6d
seonho-kim <seonho23.kim@gmail.com>
2019-04-20 00:05:10 +0900

    Step 9 - Sorting

---
+++ problem/2750/src/Main.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N;
+	static int[] array = new int[1000];
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()); 
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		N = Integer.parseInt(st.nextToken());
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			array[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		StringBuffer sb = new StringBuffer();
+		Arrays.sort(array, 0, N);
+		for (int i = 0; i < N; i++) {
+			sb.append(array[i] + "\n");
+		}
+		
+		bw.write(sb.toString() + "\n");		
+		bw.flush();
+	}
+}
