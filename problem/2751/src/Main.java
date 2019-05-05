805ffa29074d69745215d95ae4de11804c0200f9
seonho-kim <seonho23.kim@gmail.com>
2019-05-05 21:58:35 +0900

    Step 9 - Sorting [Modified mergesort]

--- problem/2751/src/Main.java
+++ problem/2751/src/Main.java
@@ -4,13 +4,13 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int N;
 	static int[] array = new int[1000000];
+	static int[] tempArray = new int[1000000];
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("./src/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -26,12 +26,47 @@ public class Main {
 		}
 		
 		StringBuffer sb = new StringBuffer();
-		Arrays.sort(array, 0, N);
+		mergeSort(0, N-1);
 		for (int i = 0; i < N; i++) {
 			sb.append(array[i] + "\n");
 		}
 		
 		bw.write(sb.toString() + "\n");		
 		bw.flush();
+	}
+	
+	static void merge(int low, int mid, int high)
+	{
+        int i = low, j = mid + 1, k = low;
+
+        while (i <= mid && j <= high)
+        {
+        	if (array[i] < array[j])
+        		tempArray[k] = array[i++];
+        	else
+        		tempArray[k] = array[j++];
+	        k++;
+        }
+
+        if (i > mid)
+        	for (int idx = j; idx <= high; idx++)
+        		tempArray[k++] = array[idx];
+        else
+             for (int idx = i; idx <= mid; idx++)
+            	 tempArray[k++] = array[idx];
+
+        for (int idx = low; idx <= high; idx++)
+        	array[idx] = tempArray[idx];
+	}
+
+	static void mergeSort(int low, int high)
+	{
+        if (high > low)
+        {
+             int mid = (low + high) / 2;
+             mergeSort(low, mid);
+             mergeSort(mid + 1, high);
+             merge(low, mid, high);
+        }
 	}
 }
