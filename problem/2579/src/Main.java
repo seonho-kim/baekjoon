5d909de8fd4db65da9bc04b0ecb7cd81674dffbe
seonho-kim <seonho23.kim@gmail.com>
2019-05-20 20:24:41 +0900

    Step 15 - Dynamic Programming 1

--- problem/2579/src/Main.java
+++ problem/2579/src/Main.java
@@ -4,16 +4,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	
-	static final int MAX_STAIR = 301;
+	static final int MAX = 301;
 	static int N;
-	static int[][] d = new int[MAX_STAIR][3];
-	static int[] v = new int[MAX_STAIR]; 
-	
+	static int[] list = new int[MAX];
+	static int[][] d = new int[MAX][3];
 	public static void main(String[] args) throws IOException {
 		System.setIn(new FileInputStream("./src/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,17 +22,17 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
-			v[i] = Integer.parseInt(st.nextToken());
+			list[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		d[1][1] = v[1];
-		d[2][1] = v[2];
-		d[2][2] = d[1][1] + v[2];
+		d[1][1] = list[1];
+		d[2][1] = list[2];
+		d[2][2] = d[1][1] + list[2];
 		for (int i = 3; i <= N; i++) {
-			d[i][1] = Math.max(d[i-2][1] + v[i], d[i-2][2] + v[i]);
-			d[i][2] = d[i-1][1] + v[i];
+			d[i][1] = Math.max(d[i-2][1] + list[i], d[i-2][2] + list[i]);
+			d[i][2] = d[i-1][1] + list[i];
 		}
-		bw.write(Integer.valueOf(Math.max(d[N][1], d[N][2])) + "\n");
+		bw.write(Math.max(d[N][1], d[N][2]) + "");
 		bw.flush();
 	}
 }
