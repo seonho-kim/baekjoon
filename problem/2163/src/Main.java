60e0864e1357e0311494b41a097c365ecacd0a75
seonho-kim <seonho23.kim@gmail.com>
2019-06-01 23:25:43 +0900

    Dynamic Programming

---
+++ problem/2163/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	static final int MAX = 301;
+	static int answer;
+	public static void main(String[] args) throws IOException { 
+		System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		answer = N*M - 1;
+		
+		bw.write(answer + "");
+		bw.flush();
+	}
+}
