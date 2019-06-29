87a0fcf743a3e0e184ca93ceac3910089012e169
seonho-kim <seonho23.kim@gmail.com>
2019-06-29 12:15:59 +0900

    New Step3 - for Statement

---
+++ problem/10950/src/Main.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int T, A, B;
+	
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		T = Integer.parseInt(br.readLine());
+		
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+		
+			bw.write((A + B) + "\n");
+			bw.flush();
+		}
+	}
+}
