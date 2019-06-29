46852547469ef77ab6d995b1045f610a1274e8ed
seonho-kim <seonho23.kim@gmail.com>
2019-06-29 12:00:23 +0900

    New Step2 - if Statement

---
+++ problem/1330/src/Main.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M;
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		if (N > M) bw.write(">");
+		else if (N < M) bw.write("<");
+		else bw.write("==");
+		bw.flush();
+	}
+}
