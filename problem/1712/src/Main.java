0de3b768dbb2787df24f7d2a8e2957467ea20adf
seonho-kim <seonho23.kim@gmail.com>
2019-07-02 21:18:47 +0900

    New Step8 - Mathematics1

---
+++ problem/1712/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int A, B, C;
+	static int answer;
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		A = Integer.parseInt(st.nextToken());
+		B = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		
+		if (B == C) answer = -1;
+		else answer = (int)(Math.round((double)A/(C-B) + 0.5));
+		
+		bw.write((answer <= 0 ? -1 : answer) + " ");
+		bw.flush();
+	}
+}
