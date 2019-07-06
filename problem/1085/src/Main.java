1f86111d9a5bad1cf01e3a1610cc00bddb30650a
seonho-kim <seonho23.kim@gmail.com>
2019-07-06 22:52:12 +0900

    New Step9 - Mathematics2

---
+++ problem/1085/src/Main.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int x, y, w, h;
+	public static void main(String[] args) throws IOException { 
+		System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		x = Integer.parseInt(st.nextToken());
+		y = Integer.parseInt(st.nextToken());
+		w = Integer.parseInt(st.nextToken());
+		h = Integer.parseInt(st.nextToken());
+		
+		x = Math.min(x, w-x);
+		y = Math.min(y, h-y);
+		
+		bw.write(Math.min(x, y) + "");
+		bw.flush();
+	}
+}
