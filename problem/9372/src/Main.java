79d98ffe1d5f76566476445a63dfd76017fc1278
seonho-kim <seonho23.kim@gmail.com>
2019-09-30 20:46:47 +0900

    Minimum Spanning Tree

---
+++ problem/9372/src/Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int T, N, M;
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			while(M-- > 0) {
+				st = new StringTokenizer(br.readLine());
+			}
+			bw.write(N-1 + "\n");
+			bw.flush();
+		}
+	}
+}
