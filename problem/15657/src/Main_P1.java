e6772ed0b1188f295447e24484c911a64d2c3d78
seonho-kim <seonho23.kim@gmail.com>
2019-12-04 20:33:47 +0900

    N and M (8)

---
+++ problem/15657/src/Main_P1.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main_P1 {
+
+	static int N, M;
+	static int[] list = new int[8], output;
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("./src/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		output = new int[M];
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			list[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(list, 0, N);
+		reCombination(N, M, 0, 0);
+		bw.flush();
+	}
+	static void reCombination(int n, int r, int idx, int target) throws IOException {
+		if (r == 0) {
+			for (int i : output) {
+				bw.write(i + " ");
+			}
+			bw.write("\n");
+			return;
+		}
+		if (target == n) return;
+
+		output[idx] = list[target];
+		reCombination(n, r-1, idx+1, target);
+		reCombination(n, r, idx, target+1);
+	}
+}
